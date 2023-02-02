4b79f2b4989ad590479cea5b7c074fb245aba53c
Gustavo Zimbron <pptavozl@gmail.com>
2023-02-02 12:24:49 -0600

    ejemplo api fetch

--- src/lib/cursos/Cursos.svelte
+++ src/lib/cursos/Cursos.svelte
@@ -1,112 +1,72 @@
-<div class="overflow-x-auto w-full">
-	<table class="table w-full">
-		<!-- head -->
-		<thead>
-			<tr>
-				<th>
-					<label>
-						<input type="checkbox" class="checkbox" />
-					</label>
-				</th>
-				<th>Nombre</th>
-				<th>Descripción</th>
-				<th />
-			</tr>
-		</thead>
-		<tbody>
-			<!-- row 1 -->
-			<tr>
-				<th>
-					<label>
-						<input type="checkbox" class="checkbox" />
-					</label>
-				</th>
-				<td>
-					<div class="flex items-center space-x-3">
-						<div class="avatar">
-							<div class="mask mask-square w-12 h-12">
-								<img src="/images/curso-online.png" alt="HTML Básico" />
+<script>
+	import Loading from '$lib/common/Loading.svelte';
+	import CursoRow from '$lib/cursos/CursoRow.svelte';
+	import { onMount } from 'svelte';
+	import { API_URL } from '../../core/constantes';
+	import { wait } from '../../core/wait';
+
+	let loading = true;
+	let cursos = [];
+
+	const cargarContenido = async () => {
+		try {
+			await wait(2000);
+
+			const result = await fetch(`${API_URL}/Cursos`);
+
+			if (result.ok) {
+				cursos = await result.json();
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			loading = false;
+		}
+	};
+
+	onMount(cargarContenido);
+</script>
+
+{#if loading}
+	<Loading />
+{:else}
+	<div class="overflow-x-auto w-full">
+		<table class="table w-full">
+			<!-- head -->
+			<thead>
+				<tr>
+					<th>
+						<label>
+							<input type="checkbox" class="checkbox" />
+						</label>
+					</th>
+					<th>Nombre</th>
+					<th>Descripción</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				{#each cursos as curso}
+					<CursoRow nombre={curso.nombre} descripcion={curso.descripcion} url={curso.url} />
+				{:else}
+					<tr>
+						<td colspan="4">
+							<div class="text-center p-10">
+								<p>No hay nada para mostrar</p>
 							</div>
-						</div>
-						<div>
-							<div class="font-bold">
-								<a
-									href="https://www.freecodecamp.org/espanol/learn/responsive-web-design/"
-									target="_blank"
-									rel="noreferrer">HTML Básico</a
-								>
-							</div>
-							<div class="text-sm opacity-50" />
-						</div>
-					</div>
-				</td>
-				<td> Curso básico de HTML en freecodecamp </td>
-			</tr>
-			<!-- row 2 -->
-			<tr>
-				<th>
-					<label>
-						<input type="checkbox" class="checkbox" />
-					</label>
-				</th>
-				<td>
-					<div class="flex items-center space-x-3">
-						<div class="avatar">
-							<div class="mask mask-square w-12 h-12">
-								<img src="/images/curso-online.png" alt="HTML Básico" />
-							</div>
-						</div>
-						<div>
-							<div class="font-bold">
-								<a
-									href="https://www.freecodecamp.org/espanol/learn/responsive-web-design/"
-									target="_blank"
-									rel="noreferrer">HtML Avanzado</a
-								>
-							</div>
-							<div class="text-sm opacity-50" />
-						</div>
-					</div>
-				</td>
-				<td>Curso avanzado de HTML en freecodecamp</td>
-			</tr>
-			<!-- row 3 -->
-			<tr>
-				<th>
-					<label>
-						<input type="checkbox" class="checkbox" />
-					</label>
-				</th>
-				<td>
-					<div class="flex items-center space-x-3">
-						<div class="avatar">
-							<div class="mask mask-square w-12 h-12">
-								<img src="/images/curso-online.png" alt="HTML Básico" />
-							</div>
-						</div>
-						<div>
-							<div class="font-bold">
-								<a
-									href="https://platzi.com/CSS-Moderno"
-									target="_blank"
-									rel="noreferrer">CSS Moderno</a
-								>
-							</div>
-							<div class="text-sm opacity-50" />
-						</div>
-					</div>
-				</td>
-				<td> CSS Moderno en Platzi </td>
-			</tr>
-		</tbody>
-		<!-- foot -->
-		<tfoot>
-			<tr>
-				<th />
-				<th>Nombre</th>
-				<th>Descripción</th>
-				<th />
-			</tr>
-		</tfoot>
-	</table>
-</div>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<!-- foot -->
+			<tfoot>
+				<tr>
+					<th />
+					<th>Nombre</th>
+					<th>Descripción</th>
+					<th />
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+{/if}
