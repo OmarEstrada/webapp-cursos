2fe6a7647d186a68c0d5f73ed2a8e7e140ff3b3b
DjPhantom02 <antonioespino2002@hotmail.com>
2023-02-23 10:15:26 -0600

    update curso y create curso

--- src/lib/prueba/Prueba.svelte
+++ src/lib/prueba/Prueba.svelte
@@ -1,13 +1,20 @@
 <script>
 	import Loading from '$lib/common/Loading.svelte';
 	import PruebaRow from './PruebaRow.svelte';
-	
 	import { onMount } from 'svelte';
 	import { API_URL } from '../../core/constantes';
 	import { wait } from '../../core/wait';
+	import Cursos from '$lib/cursos/Cursos.svelte';
 
 	let loading = true;
 	let cursos = [];
+	let activado = true;
+	let datoscursos = {
+		id: null,
+		nombre: '',
+		url: '',
+		descripcion: ''
+	};
 
 	const cargarContenido = async () => {
 		try {
@@ -27,48 +34,155 @@
 
 	onMount(cargarContenido);
 
-
 	const deleteCurso = async ({ detail: { id } }) => {
-
-		try { 
-            loading = true;
+		try {
+			loading = true;
 
 			const result = await fetch(`${API_URL}/Cursos/${id}`, {
-                method: "DELETE"
-            });
+				method: 'DELETE'
+			});
 
 			if (result.ok) {
-				cursos = cursos.filter(curso => curso.id != id);
+				cursos = cursos.filter((curso) => curso.id != id);
 			}
 		} catch (error) {
 			console.error(error);
 		} finally {
 			loading = false;
-		} 
-	}
+		}
+	};
+
+	const editCurso = async ({ detail: { id } }) => {
+		const cursoAEditar = cursos.find((curso) => curso.id == id);
+
+		if (!cursoAEditar) return;
+		datoscursos = { ...cursoAEditar };
+	};
+
+	let agregarcurso = async () => {
+		const nuevocurso = {
+			id: datoscursos.id,
+			nombre: datoscursos.nombre,
+			url: datoscursos.url,
+			descripcion: datoscursos.url
+		};
+		try {
+			loading = true;
+			const result = await fetch(`${API_URL}/Cursos`, {
+				method: 'POST',
+				body: JSON.stringify(nuevocurso),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+			if (result.ok) {
+				const nuevo = await result.json();
+
+				cursos = [...cursos, nuevo];
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			loading = false;
+		}
+	};
+
+	let actualizarcurso = async () => {
+		try {
+			loading = true;
+			const result = await fetch(`${API_URL}/Cursos/` + datoscursos.id, {
+				method: 'PUT',
+				body: JSON.stringify(datoscursos),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+			if (result.ok) {
+				cursos = cursos.map((curso) => {
+					if (curso.id != datoscursos.id) {
+						return curso;
+					}
+
+					return datoscursos;
+				});
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
 {#if loading}
 	<Loading />
 {:else}
-<h1 style="color:yellow;">PRUEBAS</h1>
+	<h1 style="color:yellow;">PRUEBAS</h1>
 	<div class="overflow-x-auto w-full">
 		<table class="table w-full">
-			<!-- head --> 
+			<!-- head -->
 			<thead>
 				<tr>
-					<th>
-						Actions
-					</th>
+					<th> Actions </th>
 					<th>Nombre</th>
 					<th>Descripción</th>
+					<th>Url</th>
 					<th />
+				</tr>
+				<tr>
+					<th>
+						<button class="btn btn-success" on:click|preventDefault={agregarcurso}
+							>AgregarCurso</button
+						>
+					</th>
+					<th>
+						<input
+							bind:value={datoscursos.nombre}
+							type="text"
+							class="from control"
+							name=""
+							id=""
+							aria-describedby="help"
+						/>
+					</th>
+					<th>
+						<input
+							bind:value={datoscursos.descripcion}
+							type="text"
+							class="from control"
+							name=""
+							id=""
+							aria-describedby="help"
+						/>
+					</th>
+					<th>
+						<input
+							bind:value={datoscursos.url}
+							type="text"
+							class="from control"
+							name=""
+							id=""
+							aria-describedby="help"
+						/>
+					</th>
+					<th>
+						<button class="btn btn-success" on:click|preventDefault={actualizarcurso}
+							>Actualizar</button
+						>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
 				{#each cursos as curso}
-					<PruebaRow id={curso.id} nombre={curso.nombre} descripcion={curso.descripcion} url={curso.url} 
-					on:deleteClicked={deleteCurso} 
+					<PruebaRow
+						id={curso.id}
+						nombre={curso.nombre}
+						descripcion={curso.descripcion}
+						url={curso.url}
+						on:deleteClicked={deleteCurso}
+						on:editarClicked={editCurso}
 					/>
 				{:else}
 					<tr>
@@ -79,7 +193,7 @@
 						</td>
 					</tr>
 				{/each}
-			</tbody> 
+			</tbody>
 		</table>
 	</div>
 {/if}
--- src/lib/prueba/PruebaRow.svelte
+++ src/lib/prueba/PruebaRow.svelte
@@ -1,55 +1,27 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-import { createEventDispatcher } from "svelte";
-	import { API_URL } from "../../core/constantes";
-const dispatch = createEventDispatcher();
+	import { goto } from '$app/navigation';
+	import { createEventDispatcher } from 'svelte';
+	import { API_URL } from '../../core/constantes';
+	const dispatch = createEventDispatcher();
 
-	export let id:string;
+	export let id: string;
 	export let nombre: string;
 	export let url: string;
-	export let descripcion: string; 
+	export let descripcion: string;
 
 	async function clickEdit() {
-	goto('prueba/edit/'+id);
-/*
-Esto se implementa en prueba/edit/XX
-		const nuevosDatos = {
-			nombre: 'Nuevo Nombre',
-			url: 'https://google.com',
-			descripcion :'Prueba de descripcion'
-		}
-		try { 
-
-			const result = await fetch(`${API_URL}/Cursos/${id}`, {
-                method: "PUT",
-				body: JSON.stringify(nuevosDatos),
-				headers:{
-					'Content-Type': 'application/json'
-				}
-            });
-
-			if (result.ok) {
-				nombre = nuevosDatos.nombre;
-				url = nuevosDatos.url;
-				descripcion = nuevosDatos.descripcion;
-				
-			}
-		} catch (error) {
-			console.error(error);
-		}
-		*/
+		dispatch('editarClicked', { id });
 	}
 	function clickDelete() {
 		//todo: confirm action
-		dispatch('deleteClicked', {id});
+		dispatch('deleteClicked', { id });
 	}
-
 </script>
 
 <tr>
 	<td>
-		<button class="btn btn-primary" on:click={clickEdit}>EDIT</button> 
-		<button class="btn btn-error" on:click={clickDelete}>DELETE</button> 
+		<button class="btn btn-primary" on:click={clickEdit}>EDIT</button>
+		<button class="btn btn-error" on:click={clickDelete}>DELETE</button>
 	</td>
 	<td>
 		<div class="flex items-center space-x-3">
@@ -68,12 +40,14 @@ Esto se implementa en prueba/edit/XX
 	</td>
 	<td>
 		{descripcion}
-		<p>Mensaje extra</p>
+	</td>
+	<td>
+		{url}
 	</td>
 </tr>
 
 <style>
-a.titulo:hover{
-color:red;
-}
+	a.titulo:hover {
+		color: red;
+	}
 </style>
